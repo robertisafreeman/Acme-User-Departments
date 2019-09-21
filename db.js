32015946a6febbd1f3b1536411f7d62aeb95fc7a
Robert Freeman <robertisafreeman@gmail.com>
2019-09-21 10:51:51 -0700

    Got my db file

---
+++ db.js
@@ -0,0 +1,69 @@
+const Sequelize = require('sequelize');
+const { UUID, UUIDV4, STRING } = Sequelize;
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_user_departments');
+
+//Models:
+
+const User = conn.define('user', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+
+    }
+});
+const Department = conn.define('department', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.UUID
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNUll: false,
+        unique: true
+    }
+});
+
+//Relationships:
+
+User.belongsTo(Department);
+Department.hasMany(User);
+
+//Sync:
+
+const syncOrSwim = async()=> {
+    await conn.sync({ force: true });
+    const departments = [
+        {name: 'Plumbing'},
+        {name: 'Sculpting'},
+        {name: 'Tech'},
+        {name: 'Management'}
+    ];
+
+    const [ Plumbing, Sculpting, Tech, Management ] = await Promise.all(departments.map(department => Department.create(department)));
+
+    const users = [
+        {name: 'Dave', departmentId: Sculpting.id},
+        {name: 'Barbara', departmentId: Tech.id},
+        {name: 'Devandra', departmentId: Plumbing.id},
+        {name: 'Felix', departmentId: Management.id}
+    ];
+
+    await Promise.all(users.map(user => User.create(user)));
+};
+
+//Export Modules/Models: 
+
+module.exports = {
+    syncOrSwim,
+    conn,
+    models: {
+        User,
+        Department
+    }
+};
